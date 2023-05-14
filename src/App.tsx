468d1b1dae1c8a3bd41aa7960c2c450b3053212a
AMO15310 <wachiraamos402@gmail.com>
2023-05-14 21:40:34 +0300

    Initialize music player project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/Navbar";
+
+const App: React.FC = ({}) => {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.scss";
+
+const NavBar: React.FC = ({}) => {
+  return (
+    <>
+      <nav className="navbar navbar-light bg-dark top-nav"></nav>
+    </>
+  );
+};
+
+export default NavBar;
