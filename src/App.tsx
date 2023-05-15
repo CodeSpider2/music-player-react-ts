7a298d048542f2b4373e5b5f9ce9f2f5666bd865
AMO15310 <wachiraamos402@gmail.com>
2023-05-15 23:17:19 +0300

    Navbar completion with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/Navbar";
+import "./index.css";
 
 const App: React.FC = ({}) => {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,16 @@ import "./navbar.scss";
 const NavBar: React.FC = ({}) => {
   return (
     <>
-      <nav className="navbar navbar-light bg-dark top-nav"></nav>
+      <nav className="navbar navbar-light bg-dark top-nav flex justify-between">
+        <div className="title text-left ml-4 text-green-500 font-bold text-lg">
+          STREAM
+        </div>
+        <div className="upgrade">
+          <button className="w-[5rem] h-[2rem] mr-8 bg-orange-400 text-white border-yellow-400 rounded-2xl border-none">
+            Upgrade
+          </button>
+        </div>
+      </nav>
     </>
   );
 };
