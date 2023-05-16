0400e3fecec9ef080144d35190d3c5430aaefb51
AMO15310 <wachiraamos402@gmail.com>
2023-05-16 21:04:18 +0300

    Remove bootstrap an sass

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import "./navbar.scss";
 
 const NavBar: React.FC = ({}) => {
   return (
     <>
-      <nav className="navbar navbar-light bg-dark top-nav flex justify-between">
-        <div className="title text-left ml-4 text-green-500 font-bold text-lg">
+      <nav className=" top-0 h-[4.5rem] flex justify-between bg-black">
+        <div className="title pt-4 text-left ml-4 text-green-500 font-bold text-lg">
           STREAM
         </div>
-        <div className="upgrade">
+        <div className="upgrade pt-4">
           <button className="w-[5rem] h-[2rem] mr-8 bg-orange-400 text-white border-yellow-400 rounded-2xl border-none">
             Upgrade
           </button>
