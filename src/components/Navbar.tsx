6cc7b70ac1a4e15205cf5014cfb1ca63339387b3
AMO15310 <wachiraamos402@gmail.com>
2023-05-22 22:55:05 +0300

    Finish sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import user_logo from "../assets/user.png";
-import music_logo from "../assets/music.png";
+import music_logo from "../assets/music-logo.png";
 
 const NavBar: React.FC = ({}) => {
   return (
     <>
-      <nav className=" top-0 h-[4.5rem] flex justify-between opacity-3 bg-blue-400 border-b-2 border-white">
-        <div className=" flex title pt-4 text-left ml-4 font-serif text-black font-bold text-lg">
+      <nav className=" top-0 h-[4.5rem] flex justify-between opacity-3 bg-gray-900 border-b-2 border-white">
+        <div className=" flex title pt-4 text-left ml-4 font-serif text-green-200 font-bold text-lg">
           STREAM
+          <img className=" ml-6 flex mb-5" src={music_logo} alt="user-logo" />
         </div>
 
         <div className="left flex gap-4">
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,11 +1,41 @@
 type SideBarProps = {};
+import home from "../assets/home-icon.png";
+import recents from "../assets/recently-played.png";
+import collections from "../assets/my-collections.png";
+import playlists from "../assets/my-playlist.png";
+import createPlaylist from "../assets/create-playlist.png";
+import music_logo from "../assets/music-logo-icon.png";
 
 const SideBar: React.FC<SideBarProps> = ({}) => {
   //   useEffect(() => {}, []);
 
   return (
     <>
-      <div className="side h-[100vh] w-[10rem] sm:w-[15rem] bg-blue-400 left-0"></div>
+      <div className="side h-[100vh] w-[10rem] sm:w-[15rem] bg-gray-900 left-0">
+        <div className="flex flex-col  items-center pl-2 text-white flex-wraptext pt-10 gap-6 h-[80%] w-[80%]">
+          <div className="home hover:bg-zinc-400 gap-3 border w-[100%] justify-center items-center flex p-2 rounded">
+            <img src={home} className="w-[25px]" alt="home" /> Home
+          </div>
+          <div className="recent gap-3 hover:bg-zinc-400  border w-[100%] justify-center items-center rounded flex p-2">
+            <img src={recents} className="w-[25px]" alt="home" />
+            Recently played
+          </div>
+          <div className="collections gap-3 hover:bg-zinc-400 border w-[100%] justify-center items-center rounded p-2 flex">
+            <img src={collections} className="w-[25px]" alt="home" />{" "}
+            Collections
+          </div>
+          <div className="playlists gap-3 hover:bg-zinc-400 border w-[100%] justify-center items-center rounded p-2 flex">
+            <img src={playlists} className="w-[25px]" alt="home" /> Playlists
+          </div>
+          <div className="create gap-3 hover:bg-zinc-400 border w-[100%] justify-center items-center rounded flex p-2">
+            <img src={createPlaylist} className="w-[25px]" alt="home" /> Create
+            playlist
+          </div>
+          <div className="music-logo flex justify-center">
+            <img src={music_logo} className="w-[100px]" alt="music-logo" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
