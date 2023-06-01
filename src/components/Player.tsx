29f928489af9b4cfbbf03efb08fb27c2b57a2e2f
AMO15310 <wachiraamos402@gmail.com>
2023-06-01 22:07:54 +0300

    Add controls to player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from "react";
-import AudioPlayer from "./AudioPlayer";
-
+import {
+  IoPlayBackSharp,
+  // IoPlayForwardSharp,
+  // IoPlaySkipBackSharp,
+  // IoPlaySkipForwardSharp,
+  // IoPlaySharp,
+  // IoPauseSharp,
+} from "react-icons/io5";
 interface PlayerProps {}
 
 const Player: React.FC<PlayerProps> = ({}) => {
@@ -9,7 +15,11 @@ const Player: React.FC<PlayerProps> = ({}) => {
   return (
     <div>
       <div className="player w-[100%] h-[7rem] bg-gray-900 fixed overflow-hidden p-0 m-0 bottom-0"></div>
-      <AudioPlayer />
+      <div className="controls flex justify-content-center items-center">
+        <button className=" bg-white">
+          <IoPlayBackSharp />
+        </button>
+      </div>
     </div>
   );
 };
