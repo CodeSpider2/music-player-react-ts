82f7d2b909afc0e0124d64c4970b9ee5766ccef1
AMO15310 <wachiraamos402@gmail.com>
2023-06-17 21:47:24 +0300

    Add cover photo

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 
-interface BoxProps {}
+interface BoxProps {
+  // name: string;
+  // cover: string;
+  // path: string;
+}
+import { MusicContext } from "../context/musicContext";
 
-const Box: React.FC<BoxProps> = ({}) => {
+const Box: React.FC<BoxProps> = () => {
+  const audioContext = useContext(MusicContext);
+  console.log(audioContext?.audioFiles[0].cover);
+
   return (
     <>
-      <div className="box w-[14rem] h-[16rem] bg-slate-300 rounded-md cursor-pointer hover:opacity-30"></div>
+      <div
+        className={
+          "box w-[14rem] h-[16rem] bg-slate-300 box-background  rounded-md cursor-pointer hover:opacity-30"
+        }
+      ></div>
     </>
   );
 };
