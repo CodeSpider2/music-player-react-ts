e528f9ba4d4ab80c2ac3876b4853f25fbe8aa12b
AMO15310 <wachiraamos402@gmail.com>
2023-07-17 13:28:58 +0300

    use API

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,28 +1,30 @@
 interface BoxProps {
-  music: string;
+  music: [];
   // name: string;
-  // cover: string;
+  cover: string;
   // path: string;
 }
 // import { MusicContext } from "../context/musicContext";
-const Box: React.FC<BoxProps> = ({ music }) => {
-  // const audioContext = useContext(MusicContext);
-  // console.log(audioContext?.audioFiles[0].cover);
-  const selectMusic = (musicName: string) => {
-    console.log(musicName);
-    localStorage.setItem("musicName", musicName);
-  };
+const Box: React.FC<BoxProps> = ({ music, cover }) => {
+  console.log(music);
+
+  // const selectMusic = (musicName: string) => {
+  // console.log(musicName);
+  // localStorage.setItem("musicName", musicName);
+  // };
   return (
     <div
       className="flex flex-col bg-slate-300"
-      onClick={() => selectMusic(music)}
+      // onClick={() => selectMusic(music)}
     >
       <div
         className={
           "box w-[14rem] h-[14rem] bg-slate-300 box-background  rounded-md cursor-pointer hover:opacity-30"
         }
-      ></div>
-      <div className="flex justify-center">{music}</div>
+      >
+        <img src={cover} className="w-[300px] h-[300px]" alt="" />
+      </div>
+      <div className="flex justify-center">{}</div>
     </div>
   );
 };
--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -1,23 +1,119 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "./Box";
 import { WidgetProps } from "../Types/types";
 import axios from "axios";
 
 const Widget: React.FC<WidgetProps> = ({}) => {
   const [music, SetMusic] = useState([]);
-  axios
-    .get("http://localhost:4500/tracks/all")
-    .then((music) => {
-      SetMusic(music.data);
+  const [cover, setCover] = useState("");
+  const [resp, setResp] = useState<any>();
+  const options = {
+    method: "GET",
+    url: "https://spotify-web2.p.rapidapi.com/artist_singles/",
+    params: { offset: "0", limit: "20", id: "2w9zwq3AktTeYYMuhMjju8" },
+    headers: {
+      "X-RapidAPI-Key": "bdaaf6df2bmshdf0727fc340485bp17dd54jsn2e8ea6d6b34d",
+      "X-RapidAPI-Host": "spotify-web2.p.rapidapi.com",
+    },
+  };
+  useEffect(() => {
+    const response = axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data.data.artist.discography.singles.items);
+        SetMusic(response.data.data.artist.discography.singles.items);
+        setCover(
+          response.data.data.artist.discography.singles.items[0].releases
+            .items[0].coverArt.sources[0].url
+        );
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    setResp(response);
+  }, []);
+
+  interface SingleItem {
+    releases: {
+      items: {
+        id: string;
+        uri: string;
+        name: string;
+        type: string;
+        date: {
+          year: number;
+          isoString: string;
+        };
+        coverArt: {
+          sources: {
+            url: string;
+            width: number;
+            height: number;
+          }[];
+        };
+        playability: {
+          playable: boolean;
+          reason: string;
+        };
+        sharingInfo: {
+          shareId: string;
+          shareUrl: string;
+        };
+        tracks: {
+          totalCount: number;
+        };
+      }[];
+    };
+  }
+
+  interface DiscographyData {
+    singles: {
+      totalCount: number;
+      items: SingleItem[];
+    };
+  }
+
+  interface ArtistData {
+    discography: DiscographyData;
+  }
+
+  interface ResponseData {
+    data: {
+      artist: ArtistData;
+    };
+    extensions: any[];
+  }
+
+  // Extracting music URLs and cover images
+  const singles: SingleItem[] = resp.data.artist.discography.singles.items;
+  const musicData = singles
+    .map((item: SingleItem) => {
+      const releases = item.releases.items;
+      if (releases.length > 0) {
+        const release = releases[0];
+        const musicUrl = release.uri;
+        const coverImage = release.coverArt.sources[0].url;
+
+        return {
+          musicUrl,
+          coverImage,
+        };
+      } else {
+        // Handle the case where releases are missing or empty
+        return null;
+      }
     })
-    .catch((error) => {
-      console.log(error);
-    });
+    .filter((item: any) => item !== null); // Remove null entries
+
+  // Pass the music URLs and cover images as props to another component
+  console.log(musicData);
+
   return (
     <div className="p-5 flex flex-wrap gap-5 pr-0 items-center">
       {music.map((music, index) => (
-        <Box key={index} music={music} />
+        <Box key={index} music={music} cover={cover} />
       ))}
+      {/* <Box music={music} cover={cover} /> */}
     </div>
   );
 };
